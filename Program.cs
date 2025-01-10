e6af4dc5574103de5299c614243ae53aac9a2dd1
d4rkc0d3r <1735522+d4rkc0d3r@users.noreply.github.com>
2025-01-10 15:36:17 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Globalization;
+
+namespace d4rkpl4y3r.Factorio.QualityCalculator;
+
+public class Program
+{
+    public static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+        Dictionary<int, (double quality, double productivity)> moduleSettings = new()
+        {
+            { 1, (0.01, 0.04) },
+            { 2, (0.02, 0.06) },
+            { 3, (0.025, 0.1) },
+        };
+        Dictionary<string, (int moduleSlots, double productivity)> machineData = new()
+        {
+            { "assembling-machine-1", (0, 0) },
+            { "assembling-machine-2", (2, 0) },
+            { "assembling-machine-3", (4, 0) },
+            { "oil-refinery", (3, 0) },
+            { "chemical-plant", (3, 0) },
+            { "centrifuge", (2, 0) },
+            { "electrical-furnace", (2, 0) },
+            { "drill", (3, 0) },
+            { "big-drill", (4, 0) },
+            { "foundry", (4, 0.5) },
+            { "bio-chamber", (4, 0.5) },
+            { "electromagnetic-plant", (5, 0.5) },
+            { "cryogenic-plant", (8, 0)},
+            
+            { "ass1" , (0, 0) },
+            { "ass2" , (2, 0) },
+            { "ass3" , (4, 0) },
+            { "ref", (3, 0) },
+            { "chem", (3, 0) },
+            { "furnace", (2, 0) },
+            { "emp", (5, 0.5) },
+            { "cryo", (8, 0) }
+        };
+        
+        var settings = QualitySettings.MyEverythingHasQualityRun;
+        (string machineName, int moduleTier, string moduleQuality) machineSettings = ("emp", 2, "unique");
+        string targetQuality = "epic";
+        bool canUseProductivity = false;
+
+        Console.WriteLine("Quality levels:");
+        for (int i = 0; i < settings.QualityLevels.Count; i++)
+        {
+            Console.WriteLine($"  {settings.QualityLevels[i].name}: {settings.QualityLevels[i].bonusLevel}, {settings.QualityLevels[i].chanceModifier*100:F1}%");
+        }
+
+        double moduleQualityFactor = 1 + settings[machineSettings.moduleQuality].bonusLevel * QualitySettings.ModuleBonusModifierPerQualityLevel;
+        var modules = moduleSettings[machineSettings.moduleTier];
+        modules.quality = Math.Floor(modules.quality * moduleQualityFactor * 1000) / 1000;
+        modules.productivity = Math.Floor(modules.productivity * moduleQualityFactor * 100) / 100;
+        Console.WriteLine($"Module quality: {modules.quality*100:F1}%");
+        Console.WriteLine($"Module productivity: {modules.productivity*100:F0}%");
+        var machine = machineData[machineSettings.machineName];
+
+        int targetQualityIndex = settings.QualityLevels.FindIndex(x => x.name == targetQuality);
+        double[] bestItemAmounts = new double[settings.QualityLevels.Count];
+        int bestProdModules = 0;
+        for (int prodModules = 0; prodModules <= machine.moduleSlots; prodModules++)
+        {
+            double qualityChance = (machine.moduleSlots - prodModules) * modules.quality;
+            double productivityBonus = machine.productivity + prodModules * modules.productivity;
+            double[] itemAmounts = new double[settings.QualityLevels.Count];
+            itemAmounts[0] = 1.0;
+
+            itemAmounts = settings.RollQualities(3 * modules.quality, 0 * modules.productivity, itemAmounts);  // mine coal
+            itemAmounts = settings.RollQualities(3 * modules.quality, 0 * modules.productivity, itemAmounts);  // craft plastic
+            itemAmounts = settings.RollQualities(qualityChance, productivityBonus, itemAmounts);               // craft red circuits
+            itemAmounts = settings.RollQualities(qualityChance, productivityBonus, itemAmounts);               // craft module
+            for (int i = 0; i < 1000; i++)
+            {
+                itemAmounts = settings.Recycle(4 * modules.quality, targetQualityIndex, itemAmounts);
+                itemAmounts = settings.RollQualities(qualityChance, productivityBonus, targetQualityIndex, itemAmounts);
+            }
+            if (itemAmounts[targetQualityIndex] > bestItemAmounts[targetQualityIndex])
+            {
+                bestItemAmounts = itemAmounts;
+                bestProdModules = prodModules;
+            }
+            if (!canUseProductivity)
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"Best module config: {machine.moduleSlots - bestProdModules} quality modules, {bestProdModules} productivity modules");
+        Console.WriteLine($"  Quality chance: {((machine.moduleSlots - bestProdModules) * modules.quality + settings.BaseChance) * 100:F2}%");
+        Console.WriteLine($"  Productivity bonus: {(machine.productivity + bestProdModules * modules.productivity) * 100:F2}%");
+        Console.WriteLine($"  Recycler quality chance: {(4 * modules.quality + settings.BaseChance) * 100:F2}% | return rate: {settings.RecyclerReturnRate * 100:F2}%");
+        Console.WriteLine($"Resulting item amounts:");
+
+        for (int i = 0; i < bestItemAmounts.Length; i++)
+        {
+            string rcpValue = bestItemAmounts[i] >= 0.25 || bestItemAmounts[i] <= 1e-50 ? "" : $"  |  ~1/{Math.Round(1/bestItemAmounts[i])}";
+            Console.WriteLine($"  {settings.QualityLevels[i].name}: {bestItemAmounts[i]*100:F2}%{rcpValue}");
+        }
+    }
+}
---
+++ QualitySettings.cs
@@ -0,0 +1,110 @@
+namespace d4rkpl4y3r.Factorio.QualityCalculator;
+
+public class QualitySettings
+{
+    public required List<(string name, int bonusLevel, double chanceModifier)> QualityLevels { get; set; }
+    public double BaseChance { get; set; } = 0.0;
+    public double RecyclerReturnRate { get; set; } = 0.25;
+    public double ProductivityCap { get; set; } = 4.0;
+
+    public const double SkipChance = 0.1;
+    public const double ModuleBonusModifierPerQualityLevel = 0.3;
+
+    public double[] RollQualities(double qualityChance, double productivityBonus, int targetLevel, double[] itemAmounts)
+    {
+        qualityChance += BaseChance;
+        double productivity = Math.Min(1 + productivityBonus, ProductivityCap);
+        var newAmounts = new double[QualityLevels.Count];
+        for (int i = 0; i < targetLevel; i++)
+        {
+            double amount = itemAmounts[i] * productivity;
+            double chance = QualityLevels[i].chanceModifier * qualityChance;
+            // code can't handle greater than 100% chance right now
+            if (chance > 1.0)
+            {
+                throw new InvalidOperationException($"Chance for quality level {QualityLevels[i].name} is {chance*100:F1}%, which is greater than 100%.");
+            }
+            for (int j = i; j < itemAmounts.Length; j++)
+            {
+                double skipToNextLevel = amount * ((j == itemAmounts.Length - 1) ? 0 : (j == i) ? chance : SkipChance);
+                newAmounts[j] += amount - skipToNextLevel;
+                amount = skipToNextLevel;
+            }
+        }
+        for (int i = targetLevel; i < itemAmounts.Length; i++)
+        {
+            newAmounts[i] += itemAmounts[i];
+        }
+        return newAmounts;
+    }
+
+    public double[] RollQualities(double qualityChance, double productivityBonus, string targetLevel, double[] itemAmounts)
+    {
+        int targetIndex = QualityLevels.FindIndex(x => x.name == targetLevel);
+        return RollQualities(qualityChance, productivityBonus, targetIndex, itemAmounts);
+    }
+
+    public double[] RollQualities(double qualityChance, double productivityBonus, double[] itemAmounts)
+    {
+        return RollQualities(qualityChance, productivityBonus, itemAmounts.Length - 1, itemAmounts);
+    }
+
+    public double[] Recycle(double qualityChance, int targetLevel, double[] itemAmounts)
+    {
+        return RollQualities(qualityChance, RecyclerReturnRate - 1, targetLevel, itemAmounts);
+    }
+
+    public double[] Recycle(double qualityChance, string targetLevel, double[] itemAmounts)
+    {
+        int targetIndex = QualityLevels.FindIndex(x => x.name == targetLevel);
+        return Recycle(qualityChance, targetIndex, itemAmounts);
+    }
+
+    public double[] Recycle(double qualityChance, double[] itemAmounts)
+    {
+        return Recycle(qualityChance, itemAmounts.Length - 1, itemAmounts);
+    }
+
+    public (string name, int bonusLevel, double chanceModifier) this[string name]
+    {
+        get
+        {
+            return QualityLevels.Find(x => x.name == name);
+        }
+    }
+
+    public static QualitySettings Vanilla = new QualitySettings
+    {
+        QualityLevels =
+        [
+            ("common", 0, 1.0),
+            ("uncommon", 1, 1.0),
+            ("rare", 2, 1.0),
+            ("epic", 3, 1.0),
+            ("legendary", 5, 1.0)
+        ]
+    };
+
+    private const double uncommonUpgradeChance = 0.1;
+    private const double upgradeChanceDecay = 0.005;
+    public static QualitySettings MyEverythingHasQualityRun = new QualitySettings
+    {
+        QualityLevels =
+        [
+            ("common", 0, 1.0),
+            ("uncommon", 2, uncommonUpgradeChance),
+            ("rare", 3, uncommonUpgradeChance - upgradeChanceDecay * 1),
+            ("epic", 4, uncommonUpgradeChance - upgradeChanceDecay * 2),
+            ("legendary", 6, uncommonUpgradeChance - upgradeChanceDecay * 3),
+            ("unique", 9, uncommonUpgradeChance - upgradeChanceDecay * 4),
+            ("mystic", 13, uncommonUpgradeChance - upgradeChanceDecay * 5),
+            ("unbreakable", 18, uncommonUpgradeChance - upgradeChanceDecay * 6),
+            ("infernal", 24, uncommonUpgradeChance - upgradeChanceDecay * 7),
+            ("demonic", 31, uncommonUpgradeChance - upgradeChanceDecay * 8),
+            ("godly", 39, 0.0)
+        ],
+        BaseChance = 0.1,
+        RecyclerReturnRate = 0.10,
+        ProductivityCap = 10.0
+    };
+}
